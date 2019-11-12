d17cac6b2004c8f6e1455e67897e99d48bbd2a2c
aparcanapavel <aparcanapavel@gmail.com>
2019-11-12 14:14:17 -0500

    Front-end user auth finished. Users can no longer access session pages once logged in

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -3,6 +3,7 @@ import GreetingContainer from './greetings/greeting_container';
 import { Route, Switch } from 'react-router-dom';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
+import { AuthRoute } from '../util/route_util';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -16,8 +17,8 @@ export default class App extends React.Component {
         <GreetingContainer />
       </header>
       
-      <Route path="/login" component={LoginFormContainer} />
-      <Route path="/signup" component={SignupFormContainer} />
+      <AuthRoute path="/login" component={LoginFormContainer} />
+      <AuthRoute path="/signup" component={SignupFormContainer} />
     </div>
   }
 }
