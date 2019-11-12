0ebee6a41b1e92a95098a1ae2165aec0e218d1fb
aparcanapavel <aparcanapavel@gmail.com>
2019-11-12 14:07:29 -0500

    Successfully have a preloaded state

--- frontend/astrogram.jsx
+++ frontend/astrogram.jsx
@@ -6,15 +6,19 @@ import Root from "./components/root";
 import {login} from './actions/session_actions';
 
 document.addEventListener("DOMContentLoaded", () => {
-
-  window.login = login;
-  window.signup = signup;
-  window.logout = logout;
-
-  const store = configureStore();
-
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
+  let store;
+  if (window.currentUser) {
+    const preloadedState = {
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser }
+      },
+      session: { id: window.currentUser.id }
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
   
   const root = document.getElementById("root");
   ReactDOM.render(<Root store={store} />, root);
