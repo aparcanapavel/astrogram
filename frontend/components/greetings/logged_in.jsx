b431774c2cce7e7cdbbceda3541c9b0b6e092fa4
aparcanapavel <aparcanapavel@gmail.com>
2019-11-14 20:04:35 -0500

    Created a modal for Image uploads. Looking good so far

--- frontend/components/greetings/logged_in.jsx
+++ frontend/components/greetings/logged_in.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+import Modal from '../modal/modal';
 
-export default class LoggedIn extends React.Component {
+class LoggedIn extends React.Component {
   constructor(props) {
     super(props);
+    this.toHome = this.toHome.bind(this);
+  }
+
+  toHome() {
+    debugger
+    this.props.history.push('/');
   }
 
   render () {
     return <nav className="nav-bar">
-      <ul>
-        <li><i className="fab fa-instagram"></i></li>
-        <li><h1 className='astrogram-title'>Astrogram</h1></li>
+      <ul className="nav-list">
+        <li onClick={this.toHome}><i className="fab fa-instagram"></i><h1 className='astrogram-nav'>Astrogram</h1></li>
         <li><input type="text" placeholder="search users"/></li>
-        <li><i className="fas fa-camera-retro"></i></li>
+        <li onClick={() => this.props.openModal('newPost')}><i className="fas fa-camera-retro"></i></li>
         <li><i className="far fa-heart"></i></li>
         <li><i className="far fa-user"></i></li>
       </ul>
+      <Modal />
     </nav>
   }
 }
+
+export default withRouter(LoggedIn);
--- frontend/components/images/image_form.jsx
+++ frontend/components/images/image_form.jsx
@@ -5,24 +5,31 @@ export default class ImageForm extends React.Component {
     super(props);
     this.state = {
       caption: "",
-      photoFile: null
+      photoFile: null,
+      photoURL: null
     }
     this.handleFile = this.handleFile.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSubmit(e){
+  handleSubmit(e) {
     e.preventDefault();
     const formData = new FormData();
     formData.append('image[caption]', this.state.caption);
     formData.append('image[photo]', this.state.photoFile);
 
-    this.props.createImage(formData);
+    return this.props.createImage(formData);
     //turn img form, add a with router on container, '.then" this.props.history.push. to the latest image id
   }
 
   handleFile(e) {
-    this.setState({ photoFile: e.currentTarget.files[0] })
+    const file = e.currentTarget.files[0];
+    const fileReader = new FileReader();
+    fileReader.onloadend = () => {
+
+      this.setState({ photoFile: file, photoURL: fileReader.result })
+    }
+    if(file) fileReader.readAsDataURL(file);
   }
 
   updateField(field) {
@@ -30,29 +37,41 @@ export default class ImageForm extends React.Component {
   }
 
   render() {
+    console.log(this.state);
     const errList = this.props.errors.map((err, i) => <li key={i}>{err}</li>);
 
-    return <section className="post-form">
-      <h3>username here</h3>
-      <form className="image-upload-form" onSubmit={this.handleSubmit}>
-        <input 
+    let preview;
+    if (this.state.photoURL) {
+      preview = <img src={this.state.photoURL} className="preview"/>
+    } else {
+      preview = <input
         type="file"
         onChange={this.handleFile}
-        />
+      />
+    } 
 
-        <label>
-          caption:
-          <input
-            type="text"
-            value={this.state.caption}
-            onChange={this.updateField("caption")}
-          />
-        </label>
+    return <section className="post-form-container">
+      <div className="post-form-modal">
+        <h3>username here</h3>
+        <form className="image-upload-form" onSubmit={this.handleSubmit}>
+          <div className="preview-upload">
+            {preview}
+          </div>
 
-        <button>Upload Image</button>
+          <label>
+            caption:
+            <input
+              type="text"
+              value={this.state.caption}
+              onChange={this.updateField("caption")}
+            />
+          </label>
 
-        <ul className="formErrors">{errList}</ul>
-      </form>
+          <button>Upload Image</button>
+
+          <ul className="formErrors">{errList}</ul>
+        </form>
+      </div>
     </section>
   }
 }
